9025c05e3a8925168ceff6e5fb7da57bedc0d341
Koki Shibata <kenergyintense@gmail.com>
2021-08-12 02:10:08 +0900

    first commit with gatsby frontend

---
+++ django_react_paypal/frontend/.eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: [
+    'import',
+    'jsx-a11y',
+    'react',
+    'react-hooks',
+  ],
+  rules: {
+    'react/prop-types': 0,
+    'import/prefer-default-export': 0,
+    'padding-line-between-statements': [
+      'error',
+      {
+        blankLine: 'always',
+        prev: '*',
+        next: 'return',
+      },
+    ],
+    'react/jsx-props-no-spreading': [
+      'error',
+      {
+        html: 'enforce',
+        custom: 'enforce',
+        explicitSpread: 'ignore',
+      },
+    ],
+  },
+};
